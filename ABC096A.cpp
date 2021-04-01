073f20869079742020dbbf99529ae3e75f119456
yichien <kevinyichienlee@gmail.com>
2021-04-02 00:04:40 +0800

    A - Day of Takahashi

---
+++ ABC096A.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int a, b;
+	cin >> a >> b;
+	if(a == b){
+		cout<< a << endl;
+	}
+	else if(a>b){
+		cout << a-1 << endl; 
+	}	
+	else if(a<b){
+		cout << a << endl;
+	}
+	return 0;
+}
+
